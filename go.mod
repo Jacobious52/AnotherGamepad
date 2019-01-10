947fd269040c24cebd93fed236c9a167cb01ec81
Sam Lewis <mazuel@gmail.com>
2019-01-10 21:53:34 +1100

    Add dir structure for server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Jacobious52/AnotherGamepad
+
+require github.com/sirupsen/logrus v1.3.0
---
+++ server/cmd/agp_server/agp_server.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	log "github.com/sirupsen/logrus"
+	// kingpin "gopkg.in/alecthomas/kingpin.v2"
+)
+
+func main() {
+	log.Infoln("Hello")
+}
